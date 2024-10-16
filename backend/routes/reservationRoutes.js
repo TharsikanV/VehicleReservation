041185b8ea75cfa2a863f08f1caa86f2729daf49
TharsikanV <108121067+TharsikanV@users.noreply.github.com>
2024-10-17 00:30:43 +0530

    add reservationRoutes

---
+++ backend/routes/reservationRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { createReservation, getReservations, deleteReservation } = require('../controllers/reservationController');
+const authenticate = require('../middleware/authMiddleware');
+const router = express.Router();
+
+router.post('/reserve', authenticate, createReservation);
+router.get('/reservations', authenticate, getReservations);
+router.delete('/reservations/:id', authenticate, deleteReservation);
+
+module.exports = router;
