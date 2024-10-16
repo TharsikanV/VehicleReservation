23840f72aaab6c1435a64fbd27816c73d41ec043
TharsikanV <108121067+TharsikanV@users.noreply.github.com>
2024-10-17 00:28:45 +0530

    add authMiddleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+const authenticate = (req, res, next) => {
+    const token = req.headers['authorization'];
+
+    if (!token) return res.status(401).json({ error: 'No token provided' });
+
+    jwt.verify(token.split(' ')[1], process.env.JWT_SECRET, (err, decoded) => {
+        if (err) return res.status(403).json({ error: 'Invalid token' });
+        req.user = decoded;
+        next();
+    });
+};
+
+module.exports = authenticate;
